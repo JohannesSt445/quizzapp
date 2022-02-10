89678e774ca0abc3de9d38453ae01bd18b2a3321
JohannesSt445 <johannes.stuemer@gmx.de>
2022-02-10 15:35:58 +0100

    .

--- login.php
+++ login.php
@@ -20,7 +20,13 @@ $m = null;
 
 
 
-require 'connect.php';
+    $tns = "quizzapp_high"; 
+
+    $user = "quizzteam2"; 
+    
+    $password = "QuizzApp9755"; 
+    
+    $conn = new PDO("oci:dbname=".$tns, $user, $password);
 
 $u = $_POST['User'];
 
