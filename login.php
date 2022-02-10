b2b16e0d8864368269b5c55d26b875c7ba9a8c74
JohannesSt445 <johannes.stuemer@gmx.de>
2022-02-10 13:31:44 +0100

    login rückgangig

--- login.php
+++ login.php
@@ -1,3 +1,67 @@
+<?php
+
+$m = null;
+
+if(isset($_POST['sub'])){
+
+
+
+require 'connect.php';
+
+$u = $_POST['User'];
+
+$p = $_POST['Passwort'];
+ 
+
+        //oracle
+
+$stid = oci_parse($conn, 'SELECT * FROM account WHERE name = :user');
+
+oci_bind_by_name($stid, ':user',$_POST['User']);
+
+
+
+oci_execute($stid);
+
+while (($row = oci_fetch_array($stid, OCI_BOTH)) != false)
+
+{
+
+ 
+
+$pass = $row[2];
+
+}
+
+ 
+
+
+
+ if($p == $pass) { // ob das eingegebene Passwort mit dem in der Datenbank übereinstimmt
+
+session_start();
+
+ $_SESSION ['Benutzer'] = $u; // Session Variable
+
+header('Location: sicher.php');
+
+ }
+
+        // wenn es nicht das Passwort ist
+
+ else {
+
+$m = '<p>Login ist fehlerhaft! Passwort oder Username ist falsch!</p>';
+
+ }
+
+}
+
+
+
+?>
+
+
 
 <!doctype html>
 
