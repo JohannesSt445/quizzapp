7f6c5e551783235c6e3248cfb9822b09401ba536
JohannesSt445 <johannes.stuemer@gmx.de>
2022-02-10 11:38:47 +0100

    erste ausgaben test

--- login.php
+++ login.php
@@ -1,22 +1,8 @@
 <?php
-require "connect.php";
+require 'connect.php';
 
-if(isset($_GET['id'])){
-    $sql = "select from kategorie where id = " . $_GET['id'];
-    $conn->query($sql);
-}
-
-$output = '';
-	$output .= '<table align="center" width="200" border="1">';
-	$output .= '<tr><th>id</th><th>id</th><th>Kategorie</th></tr>';
-	
-	foreach($pdo -> query('SELECT * FROM kategorie') as $row){
-		$output .= "<tr><td>" . $row['id'] . "</td><td>" . $row['kategorie'] . "</td><td></tr>";
-	}
-	
-	$output .= '</table>';
-	
-	echo $output;
+$sql = $conn->prepare("SELECT * FROM kategorie");
+print($sql);
 
 
 ?>
