ce49e814193a7b1d5df6a6fa18d7f8d404ba6fcb
JohannesSt445 <johannes.stuemer@gmx.de>
2022-02-10 11:33:53 +0100

    connect änderungen

--- connect.php
+++ connect.php
@@ -7,18 +7,7 @@ $password = "QuizzApp9755";
 
 
 
-try { 
 
      $conn = new PDO("oci:dbname=".$tns, $user, $password); 
-
-     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-
-     echo 'Connected to database'; 
-
-} catch(PDOException $e) { 
-
-     echo 'ERROR: ' . $e->getMessage(); 
-
-} 
 
 ?>
