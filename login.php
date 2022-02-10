3b2b1073ef305b81431c42bc64a28edf806d4c68
Danielbrs <Daniel.Boros@edu.bib.de>
2022-02-10 13:25:40 +0100

    login seite anlegen

--- login.php
+++ login.php
@@ -1,15 +1,113 @@
 <?php
+
+$m = null;
+
+if(isset($_POST['sub'])){
+
+
+
 require 'connect.php';
 
-$query = $conn->query("select * from kategorie");
-echo 'Command executed<br>'; 
+$u = $_POST['User'];
 
-  
+$p = $_POST['Passwort'];
+ 
 
-        $rows = $query->fetchAll(); 
+        //oracle
 
-  
+$stid = oci_parse($conn, 'SELECT * FROM account WHERE name = :user');
 
-        print_r($rows);
-        // hallo bitte
+oci_bind_by_name($stid, ':user',$_POST['User']);
+
+
+
+oci_execute($stid);
+
+while (($row = oci_fetch_array($stid, OCI_BOTH)) != false)
+
+{
+
+ 
+
+$pass = $row[2];
+
+}
+
+ 
+
+
+
+ if($p == $pass) { // ob das eingegebene Passwort mit dem in der Datenbank übereinstimmt
+
+session_start();
+
+ $_SESSION ['Benutzer'] = $u; // Session Variable
+
+header('Location: sicher.php');
+
+ }
+
+        // wenn es nicht das Passwort ist
+
+ else {
+
+$m = '<p>Login ist fehlerhaft! Passwort oder Username ist falsch!</p>';
+
+ }
+
+}
+
+
+
 ?>
+
+
+
+<!doctype html>
+
+<html>
+
+<head>
+
+ <title>Login</title>
+
+<meta charset= "utf-8">
+
+</head>
+
+<body>
+
+<?php 
+
+ echo $m; // Leere Zeichenkette ausgeben
+
+?>
+
+<form method="post">
+
+Username:<br>
+
+<input name="User">
+
+<br><br>
+
+Email:<br>
+
+<input name="Email">
+
+<br><br>
+
+ Passwort:<br>
+
+<input type="password" name="Passwort">
+
+<br><br>
+<input type="submit" name="sub" value="Login">
+
+<input type="button" name="sub2" value="Registrieren" onclick="location.href='sicher2.php'">
+
+</form>
+
+</body>
+
+</html>
