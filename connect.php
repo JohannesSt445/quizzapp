8a7f8182efb20bf0d246f44b2db50d580c5d2095
JohannesSt445 <johannes.stuemer@gmx.de>
2022-02-10 10:39:21 +0100

    connection to database

---
+++ connect.php
@@ -0,0 +1,24 @@
+<?php
+$tns = "quizzapp_high"; 
+
+$user = "quizzteam2"; 
+
+$password = "QuizzApp9755"; 
+
+
+
+try { 
+
+     $conn = new PDO("oci:dbname=".$tns, $user, $password); 
+
+     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+
+     echo 'Connected to database'; 
+
+} catch(PDOException $e) { 
+
+     echo 'ERROR: ' . $e->getMessage(); 
+
+} 
+
+?>
