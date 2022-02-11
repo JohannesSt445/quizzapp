745b10b4617a6caf0f2601997891d4d046cd12d9
Danielbrs <Daniel.Boros@edu.bib.de>
2022-02-11 14:33:41 +0100

    html Seiten geändert & js implementiert

---
+++ frontend/frontend.js
@@ -0,0 +1,11 @@
+var registrieren = document.querySelector('.registrieren');
+const login = document.querySelector('.login');
+const update = document.querySelector('.update');
+const logout = document.querySelector('.logout');
+
+
+signup.addEventListener('click' ,() => {location.href="./registrieren.html"})
+login.addEventListener('click' ,()=>{location.href="./login.html"})
+update.addEventListener('click', ()=>{location.href="./update.html"})
+logout.addEventListener('click' ,()=>{})
+signup.addEventListener()
---
+++ frontend/registrieren.js
@@ -0,0 +1,21 @@
+const signup = document.querySelector('input[type="submit"]')
+var status
+signup.addEventListener('click',()=>{
+  const formData = new FormData(document.querySelector('form'))
+  fetch('http://quizzapp.chickenkiller.com/backend/api.php', {
+    method: 'POST',
+    body: formData
+  })
+  .then(res => {
+    status = res.status
+    return res.text()
+  })
+  .then(data => {
+    alert(data)
+    if(status == 200)
+    location.href = "./index.html"
+
+  })
+  .catch(err => { alert(err) })
+
+})
