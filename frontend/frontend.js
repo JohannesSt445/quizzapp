2cd1d6bb5a79463b0f05ac746b659e5123a5cfcb
JohannesSt445 <johannes.stuemer@gmx.de>
2022-02-12 10:54:37 +0100

    .

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -7,5 +7,5 @@ const logout = document.querySelector('.logout');
 registrieren.addEventListener('click' ,() => {location.href="./registrieren.html"});
 login.addEventListener('click' ,()=>{location.href="./login.html"});
 update.addEventListener('click', ()=>{location.href="./update.html"});
-logout.addEventListener('click', ()=>{location.href="./logout.html"});
+logout.addEventListener('click', ()=>{location.href="logout.html"});
 
