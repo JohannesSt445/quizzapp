7410b7041785ec189e78e2a555468421e886165b
JohannesSt445 <johannes.stuemer@gmx.de>
2022-02-15 12:21:15 +0100

    .

--- backend/api.php
+++ backend/api.php
@@ -26,7 +26,7 @@ function login($conn)
     //oracle check ob Account existiert
     $sql = $conn->prepare("SELECT name, passwort, email FROM account WHERE name = ? OR email = ?");
     $sql->execute([$u, $e]);
-    $rowcount = $sql->fetchColumn();
+    $rowcount = $sql->fetch(PDO::FETCH_ASSOC);
     if ($rowcount == 0) {
         echo "Dieser Benutzer existiert nicht!";
     } else {
