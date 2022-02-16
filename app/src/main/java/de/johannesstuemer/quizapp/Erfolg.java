63d8f5747d65b6800076e8c0ef47cfba0e988fd1
Johannes Stümer <Johannes.Stuemer@edu.bib.de>
2022-02-16 15:00:33 +0100

    login and register

--- app/src/main/java/de/johannesstuemer/quizapp/Erfolg.java
+++ app/src/main/java/de/johannesstuemer/quizapp/Erfolg.java
@@ -1,4 +1,15 @@
 package de.johannesstuemer.quizapp;
 
-public class Erfolg {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Erfolg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.erflog);
+    }
 }
