dc78ec8235ed02098b92f8df46eb2ef2b4a7c1b7
Johannes Stümer <Johannes.Stuemer@edu.bib.de>
2022-02-17 15:20:40 +0100

    Revert "Merge branch 'main' of https://github.com/JohannesSt445/quizzapp"
    This reverts commit 0fa2c7e8418b3ea07b948686f9b5c1dd79755742, reversing
    changes made to 337d2559204403074162f62d04fbfed4eee5a2a4.

---
+++ app/src/main/java/de/johannesstuemer/quizapp/MainActivity.java
@@ -0,0 +1,82 @@
+package de.johannesstuemer.quizapp;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText email_et, passwort_et;
+    private String email, passwort;
+    private String URL = "http://quizzapp.chickenkiller.com/quizzapp/backend/api.php";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = passwort = "";
+        email_et = findViewById(R.id.email_et2);
+        passwort_et = findViewById(R.id.passwort_et2);
+    }
+
+    public void login(View view){
+        email = email_et.getText().toString().trim();
+        passwort = passwort_et.getText().toString().trim();
+        if(!email.equals("") && !passwort.equals("")){
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.equals("Login erfolgreich")) {
+                        Intent intent = new Intent(MainActivity.this, Erfolg.class);
+                        startActivity(intent);
+                        finish();
+                    } else if (response.equals("Login ist fehlerhaft! Passwort oder Username ist falsch!") || response.equals("Dieser Benutzer existiert nicht!")) {
+                        Toast.makeText(MainActivity.this, "Ungültiger Login Id/Passwort", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(MainActivity.this, error.toString().trim(), Toast.LENGTH_LONG).show();
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("email", email);
+                    data.put("passwort", passwort);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
+        else{
+            Toast.makeText(this, "Felder müssen ausgefüllt werden!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void register(View view){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
+        finish();
+    }
+}
