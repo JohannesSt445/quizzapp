5c4e17bd6ad51a2b938583b7420cb616c98add5d
Johannes Stümer <Johannes.Stuemer@edu.bib.de>
2022-02-18 12:59:21 +0100

    kategorie & schwierigkeit layout

---
+++ app/src/main/java/de/johannesstuemer/quizapp/Kategorie.java
@@ -0,0 +1,14 @@
+package de.johannesstuemer.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kategorie extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kategorie);
+    }
+}
